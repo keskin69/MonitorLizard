930b12a106009364d6aa1a51998bf478ed4c85e4
mustafakeskin <mustafakeskin@192.168.1.111>
2014-11-25 15:43:42 +0200

    Stable

--- src/tr/com/telekom/kmsh/Monitor.java
+++ src/tr/com/telekom/kmsh/Monitor.java
@@ -20,7 +20,7 @@ public class Monitor {
 		BasicConfigurator.configure(new NullAppender());
 
 		String confFile = "/Users/mustafakeskin/Documents/workspace/MonitorLizard/monitor.cfg";
-		String type = "-r";
+		String type = "-t";
 		String name = "Test";
 
 		if (args.length == 2) {
